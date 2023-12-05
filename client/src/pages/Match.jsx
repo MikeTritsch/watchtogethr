c3908d49c5c40b8d2130ba414e2689572cff9c16
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-12-05 14:50:17 -0600

    Added match styling.

--- client/src/pages/Match.jsx
+++ client/src/pages/Match.jsx
@@ -1,5 +1,6 @@
 import redLogo from "../assets/images/RedVersion-removebg-preview.png"
 import watchBtn from "../assets/images/icons/watch-large.png"
+import nopeBtn from "../assets/images/icons/dont-watch-large.png"
 
 const Match = () => {
   return (
@@ -16,22 +17,24 @@ const Match = () => {
             <p className="card-text">
             The music of The Beatles and the Vietnam War form the backdrop for the romance between an upper-class American girl and a poor Liverpudlian artist.
             </p>
-          </div>
           <ul className="list-group list-group-flush">
             <li className="list-group-item"><strong>Genre:</strong> Romance</li>
             <li className="list-group-item"><strong>Year:</strong> 2007</li>
             <li className="list-group-item"><strong>Starring:</strong> Jim Sturgess, Evan Rachel Wood</li>
           </ul>
+          </div>
         </div>
       </div>
-      <div className="yes-no-container">
+      <div className="gap-5 yes-no-container">
+        <div className="no-watch-btn">
+          <button className="nope-btn">
+            <img src={nopeBtn}></img>
+          </button>
+        </div>
         <div className="watch-btn">
           <button className="yes-btn">
             <img src={watchBtn}></img>
           </button>
-        </div>
-        <div className="nope-btn">
-
         </div>
       </div>
     </>
