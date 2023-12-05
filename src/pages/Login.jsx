7f3609bc04eb0c472667ee42c35cbe17de1d7ee7
martin <martinsandoval007@gmail.com>
2023-12-05 10:51:26 -0600

    add function to the sign up and login pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,37 @@
-const Login = () => {
+import { useState } from 'react';
+import {LOGIN_USER} from '../utils/mutation';
+
+
+const Login = (props) => {
+  const [formState, setFormState] = useState({email: '', password:''});
+  const [login, {error, data }] = useMutation(LOGIN_USER);
+
+  const handleChange = (e) => {
+    const {name, value } = e.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    })
+  }
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try{
+      const {data} = await login({
+        variable: {...formState },
+
+      });
+      Auth.login(data.login.token);
+    }catch (e){
+      console.error(e);
+    }
+    setFormState({
+      email: '',
+      password: '',
+    });
+  };
+
   return (
     <div>Login</div>
   )
