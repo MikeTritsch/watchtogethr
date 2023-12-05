f9c324fcb470f9836e5e403aba6a1197617a2d0d
Estevan <egonz94@gmail.com>
2023-12-05 13:20:39 -0600

    Restructured with TA Ian

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { User, Movies } = require('../models')
-const {signToken, AuthenticationError} = require ('../../src/utils/auth');
+const {signToken, AuthenticationError} = require ('../utils/auth');
 
 const resolvers = {
     Query: {
