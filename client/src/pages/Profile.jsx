1a30714e51b855545e2140f984d863393074a36c
libbyou <lcyou95@gmail.com>
2023-12-06 11:38:07 -0600

    fixed profile template

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,48 +1,51 @@
 const Profile = () => {
   return (
     <>
-      <div className="card text-center">
-        <div className="card-header">
-          <ul className="nav nav-tabs card-header-tabs">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="true" href="#myFriends">My Friends</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#myMovies">My Movies</a>
+
+      <nav>
+        <div className="nav nav-tabs" id="nav-tab" role="tablist">
+          <button className="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
+          <button className="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button>
+        </div>
+      </nav>
+      <div className="tab-content" id="nav-tabContent">
+        <div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+          <ul>
+            <li className="card mb-3">
+              <div className="card mb-3">
+                <div className="row g-0">
+                  <div className="col-md-4">
+                    <img src="..." className="img-fluid rounded-start" alt="..."/>
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h5 className="card-title">Card title 1</h5>
+                      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </li>
           </ul>
         </div>
-        <div className="card-body">
-          <ul id="myFriends">
-            <li class="card mb-3">
-                <div class="row g-0">
-                  <div class="col-md-4">
-                    <img src="..." class="img-fluid rounded-start" alt="..."/>
+        <div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+        <ul>
+            <li className="card mb-3">
+              <div className="card mb-3">
+                <div className="row g-0">
+                  <div className="col-md-4">
+                    <img src="..." className="img-fluid rounded-start" alt="..."/>
                   </div>
-                  <div class="col-md-8">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h5 className="card-title">Card title 2</h5>
+                      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                     </div>
                   </div>
                 </div>
-            </li>
-          </ul>
-          <ul id="myMovies">
-            <li class="card mb-3">
-                <div class="row g-0">
-                  <div class="col-md-4">
-                    <img src="..." class="img-fluid rounded-start" alt="..."/>
-                  </div>
-                  <div class="col-md-8">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                  </div>
-                </div>
+              </div>
             </li>
           </ul>
         </div>
