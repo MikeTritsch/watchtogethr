e83514145845bc133b113bfbcdaee6c6df75a656
Estevan <egonz94@gmail.com>
2023-12-06 07:05:58 -0600

    Changed Movie model. Added Movie component and integrated into Match page

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -1,16 +1,16 @@
 const {Schema, model} = require('mongoose');
 
 const movieSchema = new Schema({
-    id: {type: String, required: true, unique: true},
+
     title: {type: String, required: true},
     director: {type: String, required: true},
-    actors: [{
-        type: String,
-    }],
+    actors: {type: String, reuqired: true},
     year: {type: Number, required: true},
-    whereAvailable: [{
-        type: String}],
+    whereAvailable: {type: String, required: true},
+    plot: {type: String, required: true},
+    genre: {type: String, required: true}
 });
 
 const Movie = model('movie', movieSchema);
 module.exports = Movie;
+
