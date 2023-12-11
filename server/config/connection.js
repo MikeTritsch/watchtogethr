efc763fe79f52baf9d803b4a3e7a11cbebd40f3f
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-12-11 09:42:29 -0600

    Deploy .env stuff

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://watchtogethr:group2rulez@cluster0.krujsja.mongodb.net/');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = mongoose.connection;
